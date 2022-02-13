f7fa51d06f73b81887c4c498b6d05289cb9c3c3f
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-13 10:37:30 -0500

    Create ConversionEquations.py

---
+++ lib/ConversionEquations.py
@@ -0,0 +1,123 @@
+Equations = {
+	"length": {
+		"inch": {
+			"foot": "/12",
+			"yard": "/36",
+			"mile": "/63360",
+			"nanometer": "*2.54e+7",
+			"micrometer": "*25400",
+			"millimeter": "*25.4",
+			"centimeter": "*2.54",
+			"meter": "/39.37",
+			"kilometer": "/39370"
+		},
+
+		"foot": {
+			"inch": "*12",
+			"yard": "/3",
+			"mile": "/5280",
+			"nanometer": "*3.048e+8",
+			"micrometer": "*304800",
+			"millimeter": "*305",
+			"centimeter": "*30.48",
+			"meter": "/3.281",
+			"kilometer": "/3281"
+		},
+
+		"yard": {
+			"inch": "*36",
+			"foot": "*3",
+			"mile": "/1760",
+			"nanometer": "*9.144e+8",
+			"micrometer": "*914400",
+			"millimeter": "*914",
+			"centimeter": "*91.44",
+			"meter": "/1.094",
+			"kilometer": "/1094"
+		},
+
+		"mile": {
+			"inch": "*63360",
+			"foot": "*5280",
+			"yard": "*1760",
+			"nanometer": "*1.609e+12",
+			"micrometer": "*1.609e+9",
+			"millimeter": "*1.609e+6",
+			"centimeter": "*160934",
+			"meter": "*1609",
+			"kilometer": "*1.609"
+		},
+
+		"nanometer": {
+			"inch": "/2.54e+7",
+			"foot": "/3.048e+8",
+			"yard": "/9.144e+8",
+			"mile": "/1.609e+12",
+			"micrometer": "/1000",
+			"millimeter": "/1e+6",
+			"centimeter": "/1e+7",
+			"meter": "/1e+9",
+			"kilometer": "/1e+12"
+		},
+
+		"micrometer": {
+			"inch": "/25400",
+			"foot": "/304800",
+			"yard": "/914400",
+			"mile": "/1.609e+9",
+			"nanometer": "*1000",
+			"millimeter": "/1000",
+			"centimeter": "/10000",
+			"meter": "/1e+6",
+			"kilometer": "1e+9"
+		},
+
+		"millimeter": {
+			"inch": "/25.4",
+			"foot": "/305",
+			"yard": "/914",
+			"mile": "/1.609e+6",
+			"nanometer": "*1e+6",
+			"micrometer": "*1000",
+			"centimeter": "/10",
+			"meter": "/1000",
+			"kilometer": "/1e+6"
+		},
+
+		"centimeter": {
+			"inch": "/2.54",
+			"foot": "/30.48",
+			"yard": "/91.44",
+			"mile": "/160934",
+			"nanometer": "*1e+7",
+			"micrometer": "*100000",
+			"millimeter": "*10",
+			"meter": "/100",
+			"kilometer": "/100000"
+		},
+
+		"meter": {
+			"inch": "*39.37",
+			"foot": "*3.281",
+			"yard": "*1.094",
+			"mile": "/1609",
+			"nanometer": "*1e+9",
+			"micrometer": "*1e+6",
+			"millimeter": "*1000",
+			"centimeter": "*100",
+			"kilometer": "/1000"
+		},
+
+		"kilometer": {
+			"inch": "*39470",
+			"foot": "*3281",
+			"yard": "*1094",
+			"mile": "/1.609",
+			"nanometer": "*1e+12",
+			"micrometer": "*1e+9",
+			"millimeter": "*1e+6",
+			"centimeter": "*100000",
+			"kilometer": "*1000"
+		}
+	}
+}
