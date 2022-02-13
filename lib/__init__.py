9882f3c353f68e271bef4065e1c1ec5dc82ad384
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-13 10:36:27 -0500

    Create __init__.py

---
+++ lib/__init__.py
@@ -0,0 +1,8 @@
+# Import conversion dictionaries
+from lib.ConversionEquations import Equations
+from lib.ConversionTypes import Types
+from lib.ConversionVariants import Variants
+
+# Import errors
+from lib.Errors import ConversionTypeError
+from lib.Errors import NotConvertedError
