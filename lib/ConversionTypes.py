baf55a8c670e0467e5a5c90cf30b4483dbffa640
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-13 10:36:51 -0500

    Create ConversionTypes.py

---
+++ lib/ConversionTypes.py
@@ -0,0 +1,12 @@
+Types = {
+	"inch": "length",
+	"foot": "length",
+	"yard": "length",
+	"mile": "length",
+	"nanometer": "length",
+	"micrometer": "length",
+	"millimeter": "length",
+    "meter": "length",
+	"centimeter": "length",
+	"kilometer": "length",
+}
